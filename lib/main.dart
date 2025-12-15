38a46be34a3691b4beb12464d4c8ca379c4b9b03
D3STR0 <azahelsolis729@gmail.com>
2025-12-14 19:40:22 -0600

    Test plain web

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart'; // kIsWeb
-import 'package:firebase_core/firebase_core.dart';
 
-import 'firebase_options.dart';
-
-// Pantallas
-import 'ui/screens/welcome_screen.dart';
-import 'ui/screens/login_screen.dart';
-import 'ui/screens/juez/participantes_screen.dart';
-import 'ui/screens/juez/etapas_screen.dart';
-import 'ui/screens/admin/admin_dashboard_screen.dart';
-
-// Constantes
-import 'utils/constants/app_colors.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Firebase SÍ funciona en Web
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  // ❌ SQLite y sync SOLO en mobile
-  if (!kIsWeb) {
-    // importa aquí si quieres luego
-    // final dbHelper = DatabaseHelper();
-    // await dbHelper.initCalificacionesTables();
-    // SyncService().startMonitoring();
-  }
-
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Reina Nochebuena',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: AppColors.primaryBackground,
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text(
+          'SI VES ESTO, FLUTTER WEB FUNCIONA',
+          style: TextStyle(fontSize: 24),
+        ),
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (_) => const WelcomeScreen(),
-        '/login': (_) => const LoginScreen(),
-        '/participantes': (_) => const ParticipantesScreen(),
-        '/etapas': (_) => const EtapasScreen(),
-        '/admin-home': (_) => const AdminDashboardScreen(),
-      },
-    );
-  }
+    ),
+  ));
 }
